9b503bacaf69c1cad945381b48155c8d9e9b4979
Viet Vo <viet.allen.qvo@gmail.com>
2018-08-30 23:00:28 -0700

    Better wireframe ready to start scripting. Next up wireframe

---
+++ assets/javascript/main.js
@@ -0,0 +1,6 @@
+$(document.body).on("click", function() {
+   var userName = $("#userEmail").val().trim();
+   var password = $("#userPassword").val().trim();
+    console.log(userName);
+    console.log(password);
+});
